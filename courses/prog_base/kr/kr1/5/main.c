6241f6d97f096b2221b2761ed756930536020817
kate-levoshko <klevoshko98@gmail.com>
2015-10-31 12:22:57 +0200

    yjy

---
+++ courses/prog_base/kr/kr1/5/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    char str1={"a, b, c"};
+    char str2={"  a,  b,   c"};
+    char delim=(" ");
+    char a=strtok(str1, delim);
+    char b=strtok(str2, delim);
+
+    if (strlen(str1)!=strlen(str2)){
+            puts("ERROR");
+    }
+       else{
+           if (strlen(a)=strlen(b)){
+              puts("strings are equal");
+                else
+              puts("strings are NOT equal");
+           }
+        }
+    }
+}
---
+++ courses/prog_base/kr/kr1/6/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int i, n;
+    int arr[n], newArr[n];
+    scanf("%i", &n);
+    for (i=0;i<n;i++){
+        scanf("%i", &arr[i]);
+    }
+    for (i=0; i<n; i++){
+          if (i%2=o)
+        newArr[i]=arr[i];
+        printf("%i", newArr[i]);
+    }
+ return 0;
+}
