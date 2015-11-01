a8682fce8268867d47db3d929f55ca6adab5e2a2
kate-levoshko <klevoshko98@gmail.com>
2015-11-01 20:59:07 +0200

    .

---
+++ courses/prog_base/tasks/string_simple/proc.c
@@ -0,0 +1,40 @@
+#include <string.h>
+
+/*Варіант 21: У рядках записані цілі додатні числа, розділені одним пробілом.
+  Знайти рядок у якому сума цих чисел найбільша.
+  У результат записати індекс рядка, пробіл і добуток цих чисел рядка поділений на довжину рядка.
+*/
+
+
+char * process(char * resultStr, const char * textLines[],
+               int linesNum, const char * extraStr) {
+                   int sum=0, i, j, P=1;
+                   char *ptr=NULL;
+                   const char *text;
+                   int max=0, index=-1;
+                   text=textLines[i];
+
+                        for (i=0; i<linesNum; i++){
+                                ptr=strtok(text," ");
+                                sum += atoi(ptr);
+                                while (ptr!=NULL){
+                                   ptr=strtok(NULL," ");
+                                   sum += atoi(ptr);
+                                }
+
+                        }
+                        if (sum>max)
+                        {
+                            max=sum;
+                            index = i;
+                        }
+                                ptr=strtok(text[index]," ");
+                                while (ptr!=NULL){
+                                   P *= atoi(ptr);
+                                   ptr=strtok(NULL," ");
+                                }
+                                sprintf(resultStr,"%i %i", index, P/strlen(text[index]));
+                                return 0;
+                   }
+
+
