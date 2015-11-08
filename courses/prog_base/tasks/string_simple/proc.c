8458a926dacf39c977dbf1fdd5b6f642b2ed0dc7
kate-levoshko <klevoshko98@gmail.com>
2015-11-08 22:11:55 +0200

    efef

--- courses/prog_base/tasks/string_simple/proc.c
+++ courses/prog_base/tasks/string_simple/proc.c
@@ -9,6 +9,7 @@
 /*gcc - std = c89 - pedantic - errors*/
 char * process(char * resultStr, const char * textLines[], int linesNum, const char * extraStr) {
 int i, j, sum = 0, maxSum = 0, num = 0, maxIndex = 0, k = 1;
+float l;
 
 static char res[100];
 
@@ -25,16 +26,8 @@ if (maxIndex < sum){
 maxIndex = i;
 for (j = 0; j < strlen(textLines[i]); j++){
 if (isdigit(*(textLines[i] + j))){
-k = (k * (*(textLines[i] + j) - '0'))/strlen(textLines[i]);
+k = ((k * (*(textLines[i] + j) - '0')));
+l = k / strlen(textLines[1]);
 
 
-}
-}
-
-}
-}
-
-sprintf(res, "%d %d ", maxSum, k);
-return res;
-}
 }
