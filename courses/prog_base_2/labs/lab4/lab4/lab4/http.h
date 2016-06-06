b824d7a8711b1a3626668a973ce3ffacef60980d
kate-levoshko <klevoshko98@gmail.com>
2016-06-06 03:56:07 +0300

    done

---
+++ courses/prog_base_2/labs/lab4/lab4/lab4/http.h
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma once
+
+typedef struct {
+	char key[256];
+	char value[256];
+} keyvalue_t;
+
+typedef struct {
+	char method[8];
+	char uri[256];
+	keyvalue_t * form;
+	int formLength;
+} http_request_t;
+
+http_request_t
+http_request_parse(const char * const request);
+
+const char *
+http_request_getArg(http_request_t * self, const char * key);
+
+const char *
+keyvalue_toString(keyvalue_t * self);
